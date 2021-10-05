82974f662c2b002afb4bd108fdf5062d73687b4f
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2021-10-05 17:18:22 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (54.53%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -4,11 +4,13 @@ public:
     int climbStairs(int n) {
         if (n==1)
 	    	return 1;
-        int ans[n+1];
-        ans[1] = 1;
-        ans[2] = 2;
-        for(int i = 3; i<=n; i++)
-            ans[i] = ans[i-1]+ans[i-2];
-	    return ans[n];
+        int res[n+1];
+        res[0] = 0;
+        res[1]= 1;
+        res[2]=2;
+        for(int i = 3; i <= n; i++){
+            res[i] = res[i-1]+res[i-2];
+        }
+        return res[n];
     }
 };
