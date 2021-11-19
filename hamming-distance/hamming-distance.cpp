afbc8fc9ea0352ba7ab2bb74c1e07f4c89040805
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2021-11-19 16:05:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.54%) - LeetHub

--- hamming-distance/hamming-distance.cpp
+++ hamming-distance/hamming-distance.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        bitset<32>b1(x&y);
-        bitset<32>b2(x|y);
-        // count set bits in and of two numbers
-        int r1 = b1.count();
-        // count set bits in or of two numbers
-        int r2 = b2.count();
-        return abs(r1-r2);
+        // Doing XOR of both we'll get a number having set bits at positions where x and y differed, now we just need to count that.
+        int ans = 0, n = x^y;
+        // we know that right most set bit of n will be unset in n-1 and doing & of these two we turn off the right most set bit. In this process number of operations to turn of all of them will be there count. 
+        while(n){
+            n = n&n-1;
+            ans++;
+        }
+        return ans;
     }
 };
