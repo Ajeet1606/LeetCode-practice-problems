324fd90625d17f4ea9499a2747883891cdd32ca7
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-01-05 13:00:53 +0530

    Time: 36 ms (67.04%), Space: 27.5 MB (93.02%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int l = 0, r = n-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid]> target){
+                r = mid-1;
+            }
+            else
+                l = mid+1;
+        }
+        return -1;
+    }
+};
