504e60821983ec137aada4ed823c042a8c6a0c31
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-01-06 19:10:02 +0530

    Time: 16 ms (43.16%), Space: 10 MB (83.11%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    
-    bool static myCmp(const vector<int>&a, const vector<int>&b){
-        return a[1] < b[1];
-    }
-    
-    bool carPooling(vector<vector<int>>& trips, int cap) {
-        int n = trips.size();
-        unordered_map<int, int>coming, going;
-        
-        sort(trips.begin(), trips.end(), myCmp);
-        int st = trips[0][1], end = trips[0][2];
-        
-        for(int i=0; i<n; i++){
-            st = min(trips[i][1], st);
-            end = max(trips[i][2], end);
-            coming[trips[i][1]] += trips[i][0];
-            going[trips[i][2]] += trips[i][0];
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int stops[1001] = {0};
+        for(auto t: trips){
+            stops[t[1]] += t[0];
+            stops[t[2]] -= t[0];
         }
         
-        int currSize = 0;
-        for(int i=st; i<=end; i++){
-            currSize = currSize + coming[i] - going[i];
-            if(currSize > cap){
+        for(auto c: stops){
+            capacity -= c;
+            if(capacity < 0)
                 return false;
-            }
         }
-        return true;
+        
+        return capacity >= 0;
     }
 };
