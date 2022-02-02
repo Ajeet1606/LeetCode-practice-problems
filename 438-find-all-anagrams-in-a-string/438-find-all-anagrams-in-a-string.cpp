5f10369dde4ced59023ad5b271892d844afad8dd
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-02-02 19:51:59 +0530

    Time: 112 ms (9.46%), Space: 8.7 MB (76.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int freq[26] = {0};
-    string str;
-    int len;
-    bool isAnagram(int idx){
-        int a[26];
-        for(int i=0; i<26; i++)
-            a[i] = freq[i];
-        for(int i=0; i<len; i++){
-            a[str[idx++]-'a']--;
-        }
-        
+    int pp[26] = {0}, ss[26] = {0};
+    int plen, slen;
+    
+    bool isEqual(){
         for(int i=0; i<26; i++){
-            if(a[i]!= 0)
+            if(pp[i]!= ss[i])
                 return false;
         }
         return true;
     }
-    
     vector<int> findAnagrams(string s, string p) {
-        str = s;
-        len = p.length();
-        if(len > s.length()) return {};
-        for(int i=0; i<len; i++)
-            freq[p[i]-'a']++;
+        plen = p.length();
+        slen = s.length();
+        if(plen > slen) return {};
+        for(int i=0; i<plen; i++){
+            pp[p[i]-'a']++;
+            ss[s[i]-'a']++;
+        }
         
         vector<int>ans;
-        for(int i=0; i<=s.length()-len; i++){
+        if(isEqual())
+                ans.push_back(0);
+        for(int i=plen; i<s.length(); i++){
             cout<<i;
-            if(isAnagram(i))
-                ans.push_back(i);
+            
+            ss[s[i]-'a']++;
+            ss[s[i-plen]-'a']--;
+            if(isEqual())
+                ans.push_back(i-plen+1);
         }
         return ans;
     }
