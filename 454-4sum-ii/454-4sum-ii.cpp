2266bc5ed8611f4ca246f28e761cff24ce8a74b3
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-02-03 12:17:41 +0530

    Time: 332 ms (43.66%), Space: 24.6 MB (36.00%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int fourSumCount(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3, vector<int>& arr4) {
-        int n1 = arr1.size(), n2= arr2.size(), n3 = arr3.size(), n4 = arr4.size();
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
         int ans = 0;
         unordered_map<int, int>mp;
+        for(auto a: A){
+            for(auto b: B){
+                mp[a+b]++;
+            }
+        }
         
-        for(int i=0; i<n1; i++){
-            for(int j=0; j<n2; j++){
-                mp[arr1[i]+arr2[j]]++;
+        for(auto c: C){
+            for(auto d: D){
+                ans += mp[-c-d];
             }
         }
-        for(int i=0; i<n3; i++){
-            for(int j=0; j<n4; j++){
-                ans += mp[-(arr3[i]+arr4[j])];
-            }
-        }
+        
         return ans;
     }
 };
