ddac540983a16eb302434bf3115706b403dd0a0c
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-02-07 08:44:26 +0530

    Time: 4 ms (67.41%), Space: 6.6 MB (87.58%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char c = 0;
+        // Sum of ASCII values
+        char sum = t[t.length()-1];
         for(int i=0; i<s.length(); i++){
-            // c ^= s[i]; also works
-            c = (char)((int)(c) ^ (int)(s[i]));
+            sum += t[i];
+            sum -= s[i];
         }
-        for(int i=0; i<t.length(); i++){
-            c = (char)((int)(c) ^ (int)(t[i]));
-        }
-        return c;
+        return sum;
     }
 };
