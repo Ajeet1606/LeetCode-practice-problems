9cc345e5caf80e5f57708921a5d14c3db554793d
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-06-17 17:29:04 +0530

    Time: 12 ms (66.41%), Space: 6.5 MB (52.79%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,65 +2,23 @@ class Solution {
 public:
     int romanToInt(string s) {
         int len = s.length();
-        int ans = 0;
-        for(int i=0; i<len; i++){
-            //find out chars which can be subtracted and corresponding next char
-            if(s[i] == 'I'){
-                if(i+1<len){
-                    if(s[i+1]=='V'){
-                        ans += 4;
-                        i++;
-                    }
-                    else if(s[i+1]=='X'){
-                        ans += 9;
-                        i++;
-                    }else{
-                        ans +=1;
-                    }
-                }
-                else{
-                    ans +=1;
-                }
-            }else if(s[i] == 'X'){
-                if(i+1<len){
-                    if(s[i+1]=='L'){
-                        ans += 40;
-                        i++;
-                    }
-                    else if(s[i+1]=='C'){
-                        ans += 90;
-                        i++;
-                    }else{
-                        ans +=10;
-                    }
-                }
-                else{
-                    ans +=10;
-                }
-            }else if(s[i] == 'C'){
-                if(i+1<len){
-                    if(s[i+1]=='D'){
-                        ans += 400;
-                        i++;
-                    }
-                    else if(s[i+1]=='M'){
-                      ans += 900;
-                        i++;
-                    }else{
-                        ans += 100;
-                    }
-                }
-                else{
-                    ans +=100;
-                }
-            }else if(s[i] == 'V'){
-                ans += 5;
-            }else if(s[i] == 'L'){
-                ans += 50;
-            }else if(s[i] == 'D'){
-                ans += 500;
-            }else{
-                ans += 1000;
+        if(len == 0) return 0;
+        vector<int>map(24);
+        map['I'-'A'] = 1;
+        map['V'-'A'] = 5;
+        map['X'-'A'] = 10;
+        map['L'-'A'] = 50;
+        map['C'-'A'] = 100;
+        map['D'-'A'] = 500;
+        map['M'-'A'] = 1000;
+        
+        int ans = map[s[len-1] - 'A'];
+        for(int i=len-2; i>=0; i--){
+            if(map[s[i] - 'A'] < map[s[i+1] - 'A']){
+                ans -= map[s[i] - 'A'];
+            }
+            else{
+                ans += map[s[i] - 'A'];
             }
         }
         return ans;
