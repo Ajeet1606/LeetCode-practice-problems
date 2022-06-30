7af57fb854af65982daa7e8e833c78f44dcb2d7c
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-06-30 16:00:01 +0530

    Time: 13 ms (75.41%), Space: 10.8 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,28 +2,14 @@ class Solution {
 public:
     int minMoves2(vector<int>& arr) {
         int n = arr.size();
-        int ans;
-        sort(arr.begin(), arr.end());
-        
-        if(n&1){
-            int md = arr[n/2], cost = 0;
-            for(int i=0; i<n; i++){
-                cost += abs(arr[i]-md);
-            }
-            ans = cost;
+        sort(begin(arr), end(arr));
+        int i=0, j=n-1;
+        int ans = 0;
+        while(i<j){
+            ans += arr[j] - arr[i];
+            i++; 
+            j--;
         }
-        else{
-            int md1= arr[n/2], md2 = arr[n/2-1];
-            int cost1=0, cost2=0;
-            for(int i=0; i<n; i++){
-                cost1 += abs(arr[i]-md1);
-                cost2 += abs(arr[i]-md2);
-            }
-            ans = min(cost1, cost2);
-        }
-        
         return ans;
     }
 };
-
-//[203125577,-349566234,230332704,48321315,66379082,38651685 3,50986744,-250908656,-425653504,-212123143]
