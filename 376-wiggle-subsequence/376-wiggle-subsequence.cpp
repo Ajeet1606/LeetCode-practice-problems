50b6d3352a77185a141fbb0bbe6b89e19dea829a
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-03 20:56:25 +0530

    Time: 3 ms (69.26%), Space: 7.1 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,41 +2,15 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& arr) {
         int n = arr.size();
-        int answer = 1;
-        //handel n =1
-        if(n == 1) return 1;
-        // dp[i] will store the length of longest wiggle subsequence ending at ith idx.
-        vector<int>dp(n);
-        dp[0]=1;
-        //sign[i] will store the difference sign of last diff ending at ith idx.
-        vector<bool>sign(n);
-        sign[0] = true;
-        
+        int peak = 1, valley= 1;
         for(int i=1; i<n; i++){
-            for(int j=i-1; j>=0; j--){
-                if(j==0){
-                    if(arr[i] != arr[j]){
-                        dp[i] = 2;
-                        sign[i] = (arr[i]-arr[j] > 0) ? true: false;
-                    }
-                    else{
-                        dp[i] = 1;
-                        sign[i] = true;
-                    }
-                }
-                else{
-                    if(arr[i] != arr[j]){
-                        bool currSign = (arr[i]-arr[j] > 0) ? true: false;
-                        if(currSign != sign[j]){
-                            dp[i] = dp[j]+1;
-                            sign[i] = currSign;
-                            break;
-                        }   
-                    }
-                }
+            if(arr[i] > arr[i-1]){
+                peak = valley+1;
             }
-            answer = max(answer, dp[i]);
+            else if(arr[i] < arr[i-1]){
+                valley = peak+1;
+            }
         }
-        return answer;
+        return max(peak, valley);
     }
 };
