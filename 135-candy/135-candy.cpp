f9df243d52f83b63417716880451e465ce630dc1
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-04 15:17:59 +0530

    Time: 52 ms (15.52%), Space: 17.7 MB (84.26%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,22 +1,33 @@
-class Solution {
-public:
-    int candy(vector<int>& arr) {
-        int n = arr.size();
-        vector<int>left(n, 1), right(n, 1);
-        for(int i=1; i<n; i++){
-            if(arr[i] > arr[i-1]){
-                left[i] = left[i-1]+1;
+class Solution
+{
+    public:
+        int candy(vector<int> &arr)
+        {
+            int n = arr.size();
+            vector<int> candy(n, 1);
+            int ans = 0;
+
+            // forward comparsion.
+            for (int i = 1; i < n; i++){
+                if(arr[i] > arr[i-1]){
+                    candy[i] = candy[i-1]+1;
+                }
             }
-        }
-        for(int i=n-2; i>=0; i--){
-            if(arr[i] > arr[i+1]){
-                right[i] = right[i+1]+1;
+            
+            //backward comparison.
+            for(int i=n-2; i>=0; i--){
+                if(arr[i] > arr[i+1]){
+                    //might be candy[i] > candy[i+1] cz of previous change so we'll take maximum.
+                    candy[i] = max(candy[i], candy[i+1]+1);
+                }
             }
+
+            // sum up.
+            for (int i = 0; i < n; i++){
+                ans += candy[i];
+            }
+
+            return ans;
         }
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            ans += max(left[i], right[i]);
-        }
-        return ans;
-    }
 };
+// O(n) time and O(n) space.
