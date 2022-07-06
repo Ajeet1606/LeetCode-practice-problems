532a552c49e4dfffea02d3d33baf2f0fd5f03cce
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-06 07:59:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (18.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,9 @@
 class Solution
 {
     public:
-        int fib(int n)
+        int fib(int N) //Binet's Formula.
         {
-            if (n < 2) return n;
-            //memoization
-            int memo[n+1];
-            memo[0] = 0;
-            memo[1]=1;
-            for(int i=2; i<=n; i++){
-                memo[i] = memo[i-1]+memo[i-2];
-            }
-            return memo[n];
+            double phi = (sqrt(5) + 1) / 2;
+            return round(pow(phi, N) / sqrt(5));
         }
 };
