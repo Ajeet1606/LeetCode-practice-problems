6f09f6bdc093df1cd5b30433fcda51ec8057f78e
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-06 07:52:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,8 +3,14 @@ class Solution
     public:
         int fib(int n)
         {
-            if (n == 0) return n;
-            if (n == 1) return n;
-            return fib(n - 1) + fib(n - 2);
+            if (n < 2) return n;
+            //memoization
+            int memo[n+1];
+            memo[0] = 0;
+            memo[1]=1;
+            for(int i=2; i<=n; i++){
+                memo[i] = memo[i-1]+memo[i-2];
+            }
+            return memo[n];
         }
 };
