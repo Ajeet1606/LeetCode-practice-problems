98391daf6acf34498c90efa78338fe7c002efd4f
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-06 07:49:56 +0530

    Time: 17 ms (28.08%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,10 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n == 0) return n;
-        if(n == 1)return n;
-        int a = 0, b = 1;
-        int cnt = 1, c;
-        while(cnt < n){
-            c = a+b;
-            a = b;
-            b = c;
-            cnt++;
+class Solution
+{
+    public:
+        int fib(int n)
+        {
+            if (n == 0) return n;
+            if (n == 1) return n;
+            return fib(n - 1) + fib(n - 2);
         }
-        return c;
-    }
 };
