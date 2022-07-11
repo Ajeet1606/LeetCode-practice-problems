a2e77699a8bb828691771f3d1437f4451767912d
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-11 14:43:11 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (66.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -12,35 +12,21 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        //BFS with queues.
-        if(!root) return {};
-        //ans array to store result.
+        //DFS approach.
         vector<int>ans;
-        //queue to store all nodes of a particular level.
-        queue<TreeNode*>q;
-        //we start from level 1, root of tree.
-        q.push(root);
-        
-        while(!q.empty()){
-            int n = q.size();
-            // n tells the number of nodes in current level.
-            // for every level
-            for(int i=0; i<n; i++){
-                TreeNode* curr= q.front();
-                //get the node and pop from queue 
-                q.pop();
-                // if it's last node in the queue, means it's the right most of that level, it's our resultant node.
-                if(i == n-1){
-                    ans.push_back(curr->val);
-                }
-                // for this node if any child exists, push that too in queue. Notice that we don't change 'n' as size changes so we have track of curr level.
-                if(curr->left)
-                    q.push(curr->left);
-                if(curr->right)
-                    q.push(curr->right);
-                // when we re-enter in while loop now n changes and next level comes in charge. 
-            }
-        }
+        fun(root, ans, 0);
         return ans;
+    }
+    
+    //basically for every level we're traversing right child first and we push an element in array at every level.
+    //so right is called first, which is pushed and array size increases, later for same level when left child comes, array size is different so we skip that.
+    void fun(TreeNode* root, vector<int>&ans, int level){
+        if(!root) return;
+        if(ans.size() == level){
+            ans.push_back(root->val);
+        }
+        
+        fun(root->right, ans, level+1);
+        fun(root->left, ans, level+1);
     }
 };
