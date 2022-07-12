537347f13f87be93b29953401dad7ec0ca51a234
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-12 17:18:37 +0530

    Time: 20 ms (89.09%), Space: 10.1 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,48 +1,32 @@
 class Solution {
-public:
-    vector<int>arr;
-    int avgSide;
-    bool fun(vector<int>& matchsticks, int idx){
-        // base case
-        if(idx == matchsticks.size()){
-            return arr[0]==arr[1] and arr[1]==arr[2] and arr[2]==arr[3];
-        }
-        
-        for(int i=0; i<4; i++){
-            int j=i;
-            while(--j>=0){
-                if(arr[j] == arr[i])
+    bool dfs(vector<int> &sidesLength,const vector<int> &matches, int index, const int target) {
+        if (index == matches.size())
+            return sidesLength[0] == sidesLength[1] && sidesLength[1] == sidesLength[2] && sidesLength[2] == sidesLength[3];
+        for (int i = 0; i < 4; ++i) {
+            if (sidesLength[i] + matches[index] > target) // first
+                continue;
+            int j = i;
+            while (--j >= 0) // third
+                if (sidesLength[i] == sidesLength[j]) 
                     break;
-            }
-            if(j != -1) continue;
-            if(arr[i] <= avgSide){
-                arr[i] += matchsticks[idx];
-                if(fun(matchsticks, idx+1))
-                    return true;
-                arr[i] -= matchsticks[idx];
-            }
+            if (j != -1) continue;
+            sidesLength[i] += matches[index];
+            if (dfs(sidesLength, matches, index + 1, target))
+                return true;
+            sidesLength[i] -= matches[index];
         }
         return false;
     }
-    
-    bool makesquare(vector<int>& matchsticks) {
-        int n = matchsticks.size();
-        if(n==0) return false;
-        int perimeter = 0;
-        for(auto a: matchsticks){
-            perimeter += a;
+public:
+    bool makesquare(vector<int>& nums) {
+        if (nums.size() < 4) return false;
+        int sum = 0;
+        for (const int val: nums) {
+            sum += val;
         }
-        
-        avgSide = perimeter/4;
-        if(avgSide*4 != perimeter){
-            return false;
-        }
-        arr.resize(4, 0);
-        sort(matchsticks.begin(), matchsticks.end(), greater<int>());
-        for(auto a: matchsticks){
-            if(a > avgSide)
-                return false;
-        }
-        return fun(matchsticks, 0);
+        if (sum % 4 != 0) return false;
+        sort(nums.begin(), nums.end(), [](const int &l, const int &r){return l > r;}); // second
+        vector<int> sidesLength(4, 0);
+        return dfs(sidesLength, nums, 0, sum / 4);
     }
 };
