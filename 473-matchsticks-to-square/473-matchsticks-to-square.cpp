3f5c4a9f3ae8479954c5dcebfff1a5a5eaa433b6
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-12 17:03:47 +0530

    Time: 2186 ms (5.02%), Space: 10.1 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int>arr;
+    int avgSide;
+    bool fun(vector<int>& matchsticks, int idx){
+        // base case
+        if(idx == matchsticks.size()){
+            return arr[0]==arr[1] and arr[1]==arr[2] and arr[2]==arr[3];
+        }
+        
+        for(int i=0; i<4; i++){
+            int j=i-1;
+            while(j>=0){
+                if(arr[j] == arr[i])
+                    break;
+                j--;
+            }
+            if(j != -1) continue;
+            if(arr[i] <= avgSide){
+                arr[i] += matchsticks[idx];
+                if(fun(matchsticks, idx+1))
+                    return true;
+                arr[i] -= matchsticks[idx];
+            }
+        }
+        return false;
+    }
+    
+    bool makesquare(vector<int>& matchsticks) {
+        int n = matchsticks.size();
+        if(n==0) return false;
+        int perimeter = 0;
+        for(auto a: matchsticks){
+            perimeter += a;
+        }
+        
+        avgSide = perimeter/4;
+        if(avgSide*4 != perimeter){
+            return false;
+        }
+        arr.resize(4, 0);
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>());
+        return fun(matchsticks, 0);
+    }
+};
