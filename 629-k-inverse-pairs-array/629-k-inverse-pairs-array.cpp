318681c39cf1ad102030b16d5f58b87312495c6d
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-17 18:39:46 +0530

    Time: 28 ms (79.49%), Space: 6 MB (98.72%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    //time optimized, iterative approach.
-    int dp[1001][1001] = { 1 };
+    //memory optimized, iterative approach.
+    int M = 1000000007;
     int kInversePairs(int n, int k) {
-    if(dp[n][k]) return dp[n][k];
-    for (int N = 1; N <= n; ++N)
-        for (int K = 0; K <= k; ++K){
-            dp[N][K] = (dp[N-1][K] + (K>0? dp[N][K-1]: 0)) % 1000000007;
-            if(K>=N){
-                dp[N][K] = (1000000007 + dp[N][K] - dp[N-1][K-N])%1000000007;
+        int dp[2][1001] = { 1 };
+        for (int N = 1; N <= n; ++N)
+            for (int K = 0; K <= k; ++K){
+                dp[N%2][K] = (dp[(N-1)%2][K] + (K>0? dp[N%2][K-1]: 0)) % M;
+                if(K>=N){
+                    dp[N%2][K] = (M + dp[N%2][K] - dp[(N-1)%2][K-N])%M;
+                }
             }
-        }
-    return dp[n][k];
-}
+        return dp[n%2][k];
+    }
 };
