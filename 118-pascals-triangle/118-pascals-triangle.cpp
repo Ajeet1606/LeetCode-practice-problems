4278438f11c3e3eb249c577e1fca01ddf8d32c88
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-19 14:43:02 +0530

    Time: 0 ms (100.00%), Space: 7 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    //top-down dp.
     vector<vector<int>> generate(int n) {
-        //vector to store answer, n size for every row, column side will be decided dynamically.
-        vector<vector<int>>ans(n);
-        //for every row.
-        for(int i=0; i<n; i++){
-            //ith row will have i+1 elements(starting from 0), create and initialize with 1.
-            ans[i] = vector<int>(i+1, 1);
-            //we'll update mid elements [1, i-1]
-            for(int j=1; j<i; j++){
-                //every element will be sum of it's two upper elements.
-                ans[i][j] = ans[i-1][j-1]+ ans[i-1][j];
-            }
-        }
-        //we're done.
+        vector<vector<int>>ans;
+        //helper function.
+        solve(n, ans);
         return ans;
+    }
+    
+    void solve(int n, vector<vector<int>>&ans){
+        //base case, for n=0, we've to do nothing 
+        if(n == 0) return;
+        //go to top(n=1)
+        solve(n-1, ans);
+        //every step has n size array, fill with 1.
+        vector<int>row(n, 1);
+        //add in final answer array.
+        ans.push_back(row);
+        //update mid values
+        for(int i=1; i<n-1; i++){
+            //we'll do n-1, cz for nth row idx in array will be n-1.
+            ans[n-1][i] = ans[n-2][i-1]+ ans[n-2][i];
+        }
     }
 };
