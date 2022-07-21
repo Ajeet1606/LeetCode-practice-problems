1b93e149e63095a3a05a7fa9f70a1c0158289b56
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-21 16:23:34 +0530

    Time: 3 ms (72.05%), Space: 7.5 MB (63.80%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -17,6 +17,7 @@ public:
         ListNode* prev = NULL;
         //we'll reverse until we reach end or null first.
         while(head and left <= right){
+            cout<<head->val;
             ListNode *next = head->next;
             //next stores the addres of head next cz head next is changing so that we don't lose the connection of further list.
             head->next = prev;
@@ -27,6 +28,7 @@ public:
             left++;
         }
         end = head;
+        cout<<prev->val;
         //at last head becomes end and prev becomes head of reversed list..
         return prev;
     }
@@ -34,24 +36,25 @@ public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         //if list have single node.
         if(left == right) return head;
-        ListNode *curr = head;
-        ListNode *tail;
-        
-        if(left == 1){
-            tail = curr;
-            head = reverse(tail, left, right);
-        }else{
-            int cnt = 1;
-            while(curr and cnt < left-1){
-                curr = curr->next;
-                cnt++;
-            }
-            ListNode* start = curr;
-            tail = curr->next;
-            start->next = reverse(tail, left, right);
+        //we've to reverse a part of list so, something may be left before and after that part. we add a dummy node to have something before in case reverse part starts from head.
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        //curr will traverse the list.
+        ListNode *curr = dummy;
+        //we've to reach the last node of starting part.
+        int cnt = 0;
+        while(curr and cnt < left-1){
+            curr = curr->next;
+            cnt++;
         }
+        //start is the last node.
+        ListNode* start = curr;
+        //tail is the first node of reversible part, which later becomes last.
+        ListNode *tail = curr->next;
+        //so after start node, we need to reverse nodes upto right position starting from left.
+        start->next = reverse(tail, left, right);
+        //now tail is last, it'll connect the remaining part of list with itself.
         tail->next = end;
-        
-        return head;
+        return dummy->next;
     }
 };
