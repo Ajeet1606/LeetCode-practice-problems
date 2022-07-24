b758d08488d8035112049b399d3ff064f30d6fb3
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-24 07:09:41 +0530

    Time: 273 ms (21.12%), Space: 14.8 MB (88.55%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -6,6 +6,8 @@ public:
         int m = matrix.size(), n = matrix[0].size();
         
         for(int i=0; i<m; i++){
+            //as rows and cols are sorted.
+            if(matrix[i][0] > target) break;
             //search the target in ith row.
             found = search(matrix[i], 0, n-1, target);
             //if found, don't go in further rows, break;
