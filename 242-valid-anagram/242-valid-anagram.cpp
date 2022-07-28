a504a78e7e0afc8769e6dcd404135cd15a969d4b
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-07-28 13:32:40 +0530

    Time: 7 ms (87.80%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>a1(26, 0), a2(26, 0);
+        vector<int>a(26, 0);
         for(auto i: s){
-            a1[i-'a']++;
+            a[i-'a']++;
         }
         for(auto i: t){
-            a2[i-'a']++;
+            a[i-'a']--;
         }
         
         int i=0; 
-        while(i<26 and a1[i] == a2[i]){
+        while(i<26 and a[i] == 0){
             i++;
         }
         
