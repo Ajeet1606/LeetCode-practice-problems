47aac9ff7302578bcd887cb23a026a2b2a63178d
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-02 11:46:58 +0530

    Time: 39 ms (81.72%), Space: 14.2 MB (45.52%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -4,8 +4,8 @@ public:
         int n = matrix.size();
         priority_queue<int>pq;
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<n; ++j){
                 pq.push(matrix[i][j]);
                 if(pq.size() > k)
                     pq.pop();
