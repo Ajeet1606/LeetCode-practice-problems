435676b74d74aaf6b4f8b4b7a78609ce7575cb04
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-07 13:28:24 +0530

    Time: 263 ms (33.33%), Space: 83.9 MB (22.22%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     bool validPartition(vector<int>& arr) {
         int n = arr.size();
-        vector<int>dp(n+1, false);
+        //store results
+        vector<bool>dp(n+1, false);
+        
         dp[0] = true;
         for(int i=2; i<=n; i++){
+            //if prev 2 elements exist, check condition
             if(i>=2){
                 if(arr[i-1] == arr[i-2]){
                     dp[i] = dp[i] or dp[i-2];
                 }
             }
-            
+            //if prev 3 exist check this too.
             if(i>=3){
                 if(arr[i-1] == arr[i-2] and arr[i-2] == arr[i-3]){
-                    dp[i] |= dp[i-3];
+                    dp[i] = dp[i] or dp[i-3];
                 }
                 else if(arr[i-1] == arr[i-2]+1 and arr[i-2] == arr[i-3]+1){
-                    dp[i] |= dp[i-3];
+                    dp[i] = dp[i] or dp[i-3];
                 }
             }
         }
