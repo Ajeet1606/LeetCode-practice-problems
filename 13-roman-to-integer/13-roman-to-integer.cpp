1432accedacc5d47469e1a52599d50b7599b6302
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-15 11:42:56 +0530

    Time: 60 ms (5.17%), Space: 7.9 MB (40.48%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     int romanToInt(string s) {
         int ans = 0, n = s.length();
+        unordered_map<char, int>m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
         for(int i=0; i<n; i++){
             if(s[i] == 'I'){
                 if(i+1 < n and s[i+1] == 'V'){
@@ -38,10 +46,7 @@ public:
                     ans += 100;
                 }
             }else{
-                if(s[i] == 'V') ans += 5;
-                else if(s[i] == 'L') ans += 50;
-                else if(s[i] == 'D') ans += 500;
-                else if(s[i] == 'M') ans += 1000;
+                ans += m[s[i]];
             }
             cout<<ans<<" ";
         }
