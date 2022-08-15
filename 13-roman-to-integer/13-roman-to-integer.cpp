536420c471ef52805616b437bcefaec0e891c9cc
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-15 11:38:58 +0530

    Time: 16 ms (58.25%), Space: 6 MB (64.52%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int len = s.length();
-        if(len == 0) return 0;
-        vector<int>map(24);
-        map['I'-'A'] = 1;
-        map['V'-'A'] = 5;
-        map['X'-'A'] = 10;
-        map['L'-'A'] = 50;
-        map['C'-'A'] = 100;
-        map['D'-'A'] = 500;
-        map['M'-'A'] = 1000;
-        
-        int ans = map[s[len-1] - 'A'];
-        for(int i=len-2; i>=0; i--){
-            if(map[s[i] - 'A'] < map[s[i+1] - 'A']){
-                ans -= map[s[i] - 'A'];
+        int ans = 0, n = s.length();
+        for(int i=0; i<n; i++){
+            if(s[i] == 'I'){
+                if(i+1 < n and s[i+1] == 'V'){
+                    ans += 4;
+                    i++;
+                }
+                else if(i+1 < n and s[i+1] == 'X'){
+                    ans += 9;
+                    i++;
+                }else{
+                    ans +=1;
+                }
+            }else if(s[i] == 'X'){
+                if(i+1 < n and s[i+1] == 'L'){
+                    ans += 40;
+                    i++;
+                }
+                else if(i+1 < n and s[i+1] == 'C'){
+                    ans += 90;
+                    i++;
+                }
+                else{
+                    ans += 10;
+                }
+            }else if(s[i] == 'C'){
+                if(i+1 < n and s[i+1] == 'D'){
+                    ans += 400;
+                    i++;
+                }
+                else if(i+1 < n and s[i+1] == 'M'){
+                    ans += 900;
+                    i++;
+                }else{
+                    ans += 100;
+                }
+            }else{
+                if(s[i] == 'V') ans += 5;
+                else if(s[i] == 'L') ans += 50;
+                else if(s[i] == 'D') ans += 500;
+                else if(s[i] == 'M') ans += 1000;
             }
-            else{
-                ans += map[s[i] - 'A'];
-            }
+            cout<<ans<<" ";
         }
         return ans;
     }
