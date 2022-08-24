62438bc82035261a5e06c836aa23ec11c465bc47
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-24 11:34:53 +0530

    Time: 41 ms (32.12%), Space: 5.7 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n <= 0){
-           return false;
-        }
-        vector<int>arr;
-        while(n > 0){
-            int rem = n%3;
-            arr.push_back(rem);
-            n = n/3;
-        }
-        
-        int len = arr.size();
-        if(arr[len-1] != 1) return false;
-        for(int i=0; i<len-1; i++){
-            if(arr[i] != 0)
-                return false;
-        }
-        return true;
+        return n > 0 && 1162261467 % n == 0;
     }
 };
