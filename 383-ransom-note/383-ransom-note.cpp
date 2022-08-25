4c0f1861a33873dd18ac09c84fa6b54bf211a3e1
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-25 07:16:37 +0530

    Time: 16 ms (80.72%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int>m;
-        for(auto a: magazine){
-            m[a]++;
+        vector<int>arr(26, 0);
+        
+        for(int i=0; i < magazine.length(); i++){
+            arr[magazine[i]-'a']++;
         }
         
-        for(auto a: ransomNote){
-            if(m[a] > 0){
-                m[a]--;
+        for(int i=0; i < ransomNote.length(); i++){
+            if(arr[ransomNote[i]-'a'] > 0){
+                arr[ransomNote[i]-'a']--;
             }else{
                 return false;
             }
