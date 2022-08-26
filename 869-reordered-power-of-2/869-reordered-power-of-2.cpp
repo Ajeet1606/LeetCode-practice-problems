e25385b491ae2f385301f13153cb4203f1304b96
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-26 18:43:57 +0530

    Time: 3 ms (73.16%), Space: 5.9 MB (96.61%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     
-    vector<int> countDigits(int n){
-        vector<int>arr(10);
-        while(n){
-            arr[n%10]++;
-            n /= 10;
-        }
-        return arr;
-    }
-    
     bool reorderedPowerOf2(int n) {
-        vector<int>digits = countDigits(n);
+        string digits = to_string(n);
+        sort(digits.begin(), digits.end());
         
         for(int i=0, powerOf2 = 1; i<30; i++, powerOf2 <<= 1){
-            if(digits == countDigits(powerOf2))
+            string s = to_string(powerOf2);
+            sort(s.begin(), s.end());
+            if(digits == s)
                 return true;
         }
-        
-        
+               
         return false;
     }
 };
