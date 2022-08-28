084b995737ab2a274d73918c74c3e5d37dd6cd7a
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-28 13:00:09 +0530

    Time: 30 ms (13.50%), Space: 9.4 MB (28.08%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -3,43 +3,20 @@ public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
         int rows = mat.size(), cols = mat[0].size();
         //starting from bottom
-        int i=rows-1, j=0;
-        while(i>=0){
-            int r = i, c = j;
-            vector<int>a;
-            while(r<rows and c <cols){
-                a.push_back(mat[r++][c++]);
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>>pq;
+        //map<int, priority_queue<int, vector<int>, greater<int>>>pq;
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                pq[i-j].push(mat[i][j]);
             }
-            
-            sort(a.begin(), a.end());
-            r = i, c = j;
-            int idx = 0;
-            while(r<rows and c <cols){
-                mat[r++][c++] = a[idx++];
-            }
-            
-            i--;
         }
         
-        //start from left
-        i=0; j=0;
-        while(j<cols){
-            int r = i, c = j;
-            vector<int>a;
-            while(r<rows and c <cols){
-                a.push_back(mat[r++][c++]);
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                mat[i][j] = pq[i-j].top();
+                pq[i-j].pop();
             }
-            
-            sort(a.begin(), a.end());
-            r = i, c = j;
-            int idx = 0;
-            while(r<rows and c <cols){
-                mat[r++][c++] = a[idx++];
-            }
-            
-            j++;
         }
-        
         return mat;
     }
 };
