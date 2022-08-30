38106b4bf993bd47761dcae077f8fefbf6118cc5
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-30 12:14:04 +0530

    Time: 8 ms (9.58%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,53 +1,18 @@
-class Solution
-{
-    public:
-        void rotate(vector<vector < int>> &matrix)
-        {
-            int startRow = 0, endRow = matrix.size() - 1;
-            int startCol = 0, endCol = endRow;
-
-            while (startRow < endRow and startCol < endCol)
-            {
-                vector<int> v(endCol - startCol);
-               	//traverse startRow
-                for (int i = startCol, idx = 0; i < endCol; ++i)
-                {
-                    v[idx++] = matrix[startRow][i];
-                }
-
-
-               	//traverse endCol
-                for (int i = startRow, idx = 0; i < endRow; ++i)
-                {
-                    swap(matrix[i][endCol], v[idx++]);
-                }
-
-               	//traverse endRow
-                for (int i = endCol, idx = 0; i > startCol; --i)
-                {
-                    swap(matrix[endRow][i], v[idx++]);
-                }
-
-
-               	//traverse startCol
-                for (int i = endRow, idx = 0; i > startRow; --i)
-                {
-                    swap(matrix[i][startCol], v[idx++]);
-                }
-
-
-               	//traverse startRow
-                for (int i = startCol, idx = 0; i < endCol; ++i)
-                {
-                    swap(matrix[startRow][i], v[idx++]);
-                }
-
-
-                startRow++;
-                endCol--;
-                endRow--;
-                startCol++;
-               
+class Solution {
+public:
+    
+    void transpose(vector<vector<int>>&matrix){
+        int n = matrix.size();
+        for (int i = 0; i < n; ++i){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
+    }
+    
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
+        
+        transpose(matrix);
+    }
 };
