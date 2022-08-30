803e8878a5b69a52aac0ecca67ada6d503d76aab
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-08-30 12:15:01 +0530

    Time: 7 ms (17.34%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     
-    void transpose(vector<vector<int>>&matrix){
-        int n = matrix.size();
-        for (int i = 0; i < n; ++i){
-            for(int j=0; j<i; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-    }
-    
     void rotate(vector<vector<int>>& matrix) {
-        reverse(matrix.begin(), matrix.end());
-        
-        transpose(matrix);
+        int n = matrix.size();
+        int a = 0;
+        int b = n-1;
+        while(a<b){
+            for(int i=0;i<(b-a);++i){
+                swap(matrix[a][a+i], matrix[a+i][b]);
+                swap(matrix[a][a+i], matrix[b][b-i]);
+                swap(matrix[a][a+i], matrix[b-i][a]);
+            }
+            ++a;
+            --b;
+        }
     }
 };
