ee5a71176f1665b6bdc72695eebdaf33c2a24788
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-05 07:43:29 +0530

    Time: 61 ms (5.44%), Space: 12.5 MB (6.02%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>>m;
+    vector<vector<int>>ans;
     
     void dfs(Node* root, int level){
-        if(root->children.size() == 0){
-            m[level].push_back(root->val);
+        if(root == NULL){
             return;
         }
+        if(level == ans.size())
+            ans.emplace_back();
         
+        ans[level].push_back(root->val);
         vector<Node*>child = root->children;
         for(auto c: child){
             dfs(c, level+1);
         }
-        m[level].push_back(root->val);
     }
     
     vector<vector<int>> levelOrder(Node* root) {
         if(!root) return {};
         
         dfs(root, 0);
-        vector<vector<int>>ans;
-        for(int i=0; i<m.size(); i++){
-            ans.push_back(m[i]);
-        }
+        
         return ans;
     }
 };
