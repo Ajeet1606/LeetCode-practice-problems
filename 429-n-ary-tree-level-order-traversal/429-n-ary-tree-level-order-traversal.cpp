989da1a61f882e6902ca5265ee533d702162e18f
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-05 07:34:22 +0530

    Time: 60 ms (5.44%), Space: 12.6 MB (6.02%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -1,46 +1,28 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
 class Solution {
 public:
-    vector<vector<int>> levelOrder(Node* root) {
-        if(!root) return {};
-        vector<vector<int>>ans;
-        queue<Node*>q;
-        q.push(root);
-        while(!q.empty()){
-            int n = q.size();
-            vector<int>a;
-            while(n--){
-                Node* node = q.front();
-                q.pop();
-                int v = node->val;
-                vector<Node*>child = node->children;
-                a.push_back(v);
-                for(auto a: child){
-                    q.push(a);
-                }
-            }
-            ans.push_back(a);
+    unordered_map<int, vector<int>>m;
+    
+    void dfs(Node* root, int level){
+        if(root->children.size() == 0){
+            m[level].push_back(root->val);
+            return;
         }
         
+        vector<Node*>child = root->children;
+        for(auto c: child){
+            dfs(c, level+1);
+        }
+        m[level].push_back(root->val);
+    }
+    
+    vector<vector<int>> levelOrder(Node* root) {
+        if(!root) return {};
+        
+        dfs(root, 0);
+        vector<vector<int>>ans;
+        for(int i=0; i<m.size(); i++){
+            ans.push_back(m[i]);
+        }
         return ans;
     }
 };
