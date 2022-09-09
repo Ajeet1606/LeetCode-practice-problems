9619f2ce99f43657b095cb5e8354f7911d1dd223
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-09 08:54:48 +0530

    Time: 995 ms (60.61%), Space: 125.5 MB (42.40%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    static bool comp(vector<int>&a, vector<int>&b){
-        if(a[0] == b[0]){
-            return a[1] > b[1];
-        }
-        return a[0] < b[0];
-    }
-    
-    
     int numberOfWeakCharacters(vector<vector<int>>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end(), comp);
-        int ans = 0, mx = INT_MIN;
-        for(int i=n-1; i>=0; i--){
-            if(arr[i][1] < mx){
-                ans++;
-            }else{
-                mx = max(mx, arr[i][1]);
-            }
+        int max_d[100002] = {}, res = 0;
+        
+        for(auto &a: arr){
+            max_d[a[0]] = max(max_d[a[0]], a[1]);
         }
-        return ans;
+        
+        for(int i=100000; i>0; i--){
+            max_d[i-1] = max(max_d[i-1], max_d[i]);
+        }
+        
+        for(auto &a: arr){
+            res += max_d[a[0]+1] > a[1];
+        }
+        
+        return res;
     }
 };
