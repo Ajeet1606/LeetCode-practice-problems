1c6b6317f1de7fa84d0cc48a8198a0ea9d2853fd
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-13 12:46:23 +0530

    Time: 41 ms (10.22%), Space: 14 MB (38.65%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     bool validUtf8(vector<int>& data) {
-        int ones = 0;
-        int mask1 = 1<<7;   //10000000
-        int mask2 = 1<<6;   // 1000000
-        
-        for(int i=0; i<data.size(); i++){
-            if(ones == 0){
-                int mask = 1 << 7;
-                while((mask & data[i]) != 0){
-                    ones++;
-                    mask = mask >> 1;
-                }
-                
-                if(ones == 0) continue;
-                
-                if(ones > 4 or ones == 1) return false;
+        int count = 0;
+        for (auto c : data) {
+            if (count == 0) {
+                if ((c >> 5) == 0b110) count = 1;
+                else if ((c >> 4) == 0b1110) count = 2;
+                else if ((c >> 3) == 0b11110) count = 3;
+                else if ((c >> 7)) return false;
+            } else {
+                if ((c >> 6) != 0b10) return false;
+                count--;
             }
-            else{
-                if(!((data[i] & mask1) != 0 && (mask2 & data[i]) == 0)){
-                    return false;
-                }
-            }
-            ones--;
         }
-        
-        return ones == 0;
+        return count == 0;
     }
 };
