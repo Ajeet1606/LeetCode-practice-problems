46c0d01d521ce09799626e47d9f794a01fc7d224
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-13 12:42:06 +0530

    Time: 17 ms (66.58%), Space: 14 MB (38.65%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,60 +1,30 @@
 class Solution {
 public:
-    
-    string getBinary(int num){
-        string ans = "00000000";
-        int i=7;
-        while(i>=0){
-            char c = (num%2) + '0';
-            ans[i--] = c;
-            num /= 2;
-        }
-        return ans;
-    }
-    
     bool validUtf8(vector<int>& data) {
-        int n = data.size();
-        //vector<string>v(n);
+        int ones = 0;
+        int mask1 = 1<<7;   //10000000
+        int mask2 = 1<<6;   // 1000000
         
-        // for(int i=0; i<n; i++){
-        //     int num = data[i];
-        //     v[i] = getBinary(num);
-        //     //cout<<v[i]<<" ";
-        // }
-        bool flag = true;
-        //check for UTF-8 validation
-        for(int i=0; i<n; i++){
-            //get starting string
-            string str = getBinary(data[i]);
-            
-            //find count bit chars
-            int ones = 0, j=0;
-            while(j<8 and str[j++] == '1') ones++;
-            
-            //it's 'ones' bytes character 
-            if(j >= 8 or ones == 1 or ones > 4 or ones-1 + i >= n){
-                flag = false;
-                break;
+        for(int i=0; i<data.size(); i++){
+            if(ones == 0){
+                int mask = 1 << 7;
+                while((mask & data[i]) != 0){
+                    ones++;
+                    mask = mask >> 1;
+                }
+                
+                if(ones == 0) continue;
+                
+                if(ones > 4 or ones == 1) return false;
             }
-            while(ones > 1){
-                string s = getBinary(data[++i]);
-                if(s[0] != '1' or s[1] != '0') break;
-                ones--;
+            else{
+                if(!((data[i] & mask1) != 0 && (mask2 & data[i]) == 0)){
+                    return false;
+                }
             }
-            if(ones > 1){
-                flag = false;
-                break;
-            }
+            ones--;
         }
         
-        return flag;
+        return ones == 0;
     }
 };
-
-
-
-
-
-
-
-
