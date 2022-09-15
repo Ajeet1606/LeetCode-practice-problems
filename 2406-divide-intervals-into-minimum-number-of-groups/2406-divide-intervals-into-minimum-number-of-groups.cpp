867c4b96c1295f30f6fcc5d0e4e539cdd83f7ff8
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-15 19:50:35 +0530

    Time: 1262 ms (24.67%), Space: 101 MB (52.02%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        int n = 1000011;
-        vector<int>a(n, 0);
-        for(auto x: intervals){
-            a[x[0]]++;
-            a[x[1]+1]--;
-        }
+        priority_queue<int, vector<int>, greater<int>> pq;
         
-        //prefix sum
-        for(int i=1; i<n-1; i++){
-            a[i] += a[i-1];
+        sort(intervals.begin(), intervals.end()); 
+        int ans=0;
+        for (auto inter: intervals) {
+            int start= inter[0], end= inter[1]; 
+            if (pq.size() < 1 or pq.top() > start) 
+                ans++; 
+            else pq.pop(); 
+            pq.push(end+1);
         }
-        
-        int ans = 0;
-        for(auto i: a)
-            ans = max(ans, i);
         return ans;
     }
 };
