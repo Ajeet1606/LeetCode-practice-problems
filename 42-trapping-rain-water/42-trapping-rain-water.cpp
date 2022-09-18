a5ed04d86107410f942057d394d70eba1dedaec3
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-18 16:58:07 +0530

    Time: 14 ms (94.18%), Space: 19.7 MB (81.79%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,21 +1,27 @@
-//Better approach O(n) and O(n)
+//Optimal approach O(n) and O(1)
 class Solution {
 public:
     int trap(vector<int>& height) {
         int ans = 0;
         int n = height.size();
         
-        vector<int>prefix(n, 0), suffix(n, 0);
-        prefix[0] = height[0];
-        for(int i=1; i<n; i++)
-            prefix[i] = max(prefix[i-1], height[i]);
+        int l = 0, r = n-1;
+        int leftMx = 0, rightMx = 0;
         
-        suffix[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--)
-            suffix[i] = max(suffix[i+1], height[i]);
-        
-        for(int i=0; i<n; i++){            
-            ans += min(prefix[i], suffix[i]) - height[i];
+        while(l <= r){
+            if(height[l] <= height[r]){
+                if(height[l] > leftMx)
+                    leftMx = height[l];
+                else
+                    ans += leftMx - height[l];
+                l++;
+            }else{
+                if(height[r] >= rightMx)
+                    rightMx = height[r];
+                else
+                    ans += rightMx - height[r];
+                r--;
+            }
         }
         
         return ans;
