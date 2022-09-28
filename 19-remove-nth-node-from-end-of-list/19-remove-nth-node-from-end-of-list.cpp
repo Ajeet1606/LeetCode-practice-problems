249d57a3d75c37000dfa5482d75a45767b5a27e2
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-28 08:25:58 +0530

    Two pass solution and comments added.

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -8,24 +8,66 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//-------------------------------------- TWO PASS ------------------------------------------------>
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int len = 0;
+        ListNode* curr = head;
+        //calculate length of list
+        while(curr != NULL){
+            len++;
+            curr = curr->next;
+        }
+      
+        //find target node number
+        int node_number_from_start = len - n + 1;
+        //if it's head, delete this
+        if(node_number_from_start == 1){
+            return head->next;
+        }
+        //move to nearest of target node
+        curr = head;
+        int i=2; 
+        while(i< node_number_from_start){
+            curr = curr->next;
+            i++;
+        }
+        //update that with next of target
+        curr->next = curr->next->next;
+        return head;
+    }
+};
+
+
+
+
+//---------------------------------------ONE PASS----------------------------------------------------->
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //add a dummy node in starting.
         ListNode* dummy = new ListNode(0);
+        //connect with head
         dummy->next = head;
+        //we start from dummy
         ListNode* front = dummy, *back = dummy;
+        //move front pointer n steps
         while(front != NULL and n--){
             front = front->next;
         }
-        
-        bool delete_first = true;
+        //now, untill front reaches last node, we move back with it, if it is already reached, means head is to deleted, return head->next.
+        bool delete_first = true;   //flag to check if head is deleted
         while(front->next != NULL){
             front = front->next;
             back = back->next;
             delete_first = false;
         }
+        //head deleted condition
         if(delete_first) return head->next;
-        
+        //remove targeted node
         back->next = back->next->next;
         return head;
     }
