04025220201e2b343722e4be65ed48e25b43b16d
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-09-29 08:45:29 +0530

    Time: 110 ms (50.59%), Space: 31.6 MB (75.84%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
+        int l = 0, r = arr.size()-1;
         vector<int>ans;
         
-        unordered_map<int, vector<int>>map;
-        int mn_diff = INT_MAX, mx_diff = INT_MIN;
-        
-        for(int i=0; i<n; i++){
-            int diff = abs(x - arr[i]);
-            map[diff].push_back(arr[i]);
-            mn_diff = min(mn_diff, diff);
-            mx_diff = max(mx_diff, diff);
-        }
-        //cout<<mn_diff<<' '<<mx_diff;
-        int nums_taken = 0;
-        for(int i= mn_diff; i<= mx_diff; i++){
-            for(auto a: map[i]){
-                ans.push_back(a);
-                nums_taken++;
-                if(nums_taken == k) break;
+        while(r - l + 1 > k){
+            if(abs(x - arr[l]) <= abs(x - arr[r])){
+                r--;
+            }else{
+                l++;
             }
-            if(nums_taken == k) break;
         }
-        sort(ans.begin(), ans.end());
+        
+        for(int i=l; i<=r; i++)
+            ans.push_back(arr[i]);
         return ans;
     }
 };
