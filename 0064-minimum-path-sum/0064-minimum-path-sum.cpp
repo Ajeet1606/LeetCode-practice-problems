5bd2b7fc3bb127a1a9fc3e346a9fae3ae093b48c
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-10-14 16:27:19 +0530

    Time: 15 ms (67.41%), Space: 10.1 MB (56.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,33 +3,35 @@ class Solution
 {
     public:
 
-       	//------------------------------Tabulation--------------------------
-       	//					   O(rc) and O(rc)
+       	//------------------------------Space optimized---------------
+       	//					   O(rc) and O(c)
 
         int minPathSum(vector<vector < int>> &grid)
         {
             int rows = grid.size(), cols = grid[0].size();
-            vector<vector < int>> dp(rows, vector<int> (cols, 0));
+            vector<int> prev(cols, 0);
 
             for (int i = 0; i < rows; ++i)
             {
+                vector<int> curr(cols, 0);
                 for (int j = 0; j < cols; ++j)
                 {
-                    if (i == 0 and j == 0) dp[i][j] = grid[0][0];
+                    if (i == 0 and j == 0) curr[j] = grid[0][0];
                     else
                     {
                         int up = 1e9;
                         if (i > 0)
-                            up = dp[i - 1][j];
+                            up = prev[j];
                         int left = 1e9;
                         if (j > 0)
-                            left = dp[i][j - 1];
+                            left = curr[j - 1];
 
-                        dp[i][j] = grid[i][j] + min(up, left);
+                        curr[j] = grid[i][j] + min(up, left);
                     }
                 }
+                prev = curr;
             }
 
-            return dp[rows - 1][cols - 1];
+            return prev[cols - 1];
         }
 };
