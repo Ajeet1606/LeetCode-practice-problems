6eeea24ca422ba7b48f52a89ec6b24b07201731b
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-10-23 18:51:05 +0530

    Time: 42 ms (84.57%), Space: 21.5 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,19 +3,19 @@ public:
     vector<int> findErrorNums(vector<int>& arr) {
         int n = arr.size();
         
-        int actual_sum = n*(n+1)/2;
-        int curr_sum = accumulate(arr.begin(), arr.end(), 0);
-        int duplicate;
-        unordered_set<int>s;
-        for(auto a: arr){
-            if(s.count(a) != 0){
-                duplicate = a;
-                break;
+        sort(arr.begin(), arr.end());
+        int dup = -1, missing = 1;
+        for(int i=1; i<n; i++){
+            if(arr[i] == arr[i-1]){
+                dup = arr[i];
             }
-            else
-                s.insert(a);
+            else if(arr[i] > arr[i-1]+1){
+                missing = arr[i-1]+1;
+            }
         }
         
-        return {duplicate, actual_sum - curr_sum + duplicate};
+        if(arr[n-1] != n)
+            missing = n;
+        return {dup, missing};
     }
 };
