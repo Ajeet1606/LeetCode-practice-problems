47c11bef3fe5f5674383dda2c6b228363ac27ecc
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-12-08 19:49:38 +0530

    Time: 508 ms (47.77%), Space: 92.9 MB (55.91%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     
-    long long dp[2][100001];
     long long maxAlternatingSum(vector<int>& arr) {
         int n = arr.size();
-        memset(dp, -1, sizeof(dp));
-        return fun(arr, n, 0, 0);
-    }
-    
-    long long fun(vector<int>&arr, int n, int cur, int pos){
-        if(cur >= n) return 0;
+        long long dp[n][2];
+        memset(dp, 0, sizeof(dp));
         
-        if(dp[pos][cur] != -1) return dp[pos][cur];
-        //pick cur
-        //if pos is (true)even add in sum
-        //else subtract from sum.
-                
-        long long pick, skip;
-        if(pos == 0)
-            pick = fun(arr, n, cur+1, 1) + arr[cur];
-        else
-            pick = fun(arr, n, cur+1, 0) - arr[cur];
+        dp[0][0] = arr[0];
         
-        //skip cur
+        for(int i=1; i<n; i++){
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + arr[i]);
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - arr[i]);
+        }
         
-        skip = fun(arr, n, cur+1, pos);
-        
-        return dp[pos][cur] = max(pick, skip);
-    }
+        return max(dp[n-1][0], dp[n-1][1]);
+    }    
 };
