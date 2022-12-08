534d2ff0eddd77eab115df37ea69abbb049d19bd
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2022-12-08 19:51:16 +0530

    Time: 410 ms (67.19%), Space: 91.2 MB (72.18%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     
-    long long maxAlternatingSum(vector<int>& arr) {
-        int n = arr.size();
-        long long dp[n][2];
-        memset(dp, 0, sizeof(dp));
-        
-        dp[0][0] = arr[0];
-        
-        for(int i=1; i<n; i++){
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + arr[i]);
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - arr[i]);
+    long long maxAlternatingSum(vector<int>& A) {
+        long long even = A[0], odd = 0;
+      
+        for(int i=1; i<A.size(); ++i){
+            long long newEven = max(even, odd + A[i]);
+            odd = max(odd, even - A[i]);
+            even = newEven;
         }
-        
-        return max(dp[n-1][0], dp[n-1][1]);
+   
+        return max(even, odd);
     }    
 };
