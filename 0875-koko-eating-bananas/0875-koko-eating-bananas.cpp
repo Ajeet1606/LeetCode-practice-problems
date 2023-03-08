47faa0439bc0b7d173131bb8b09a9c05331897cb
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2023-03-08 16:18:56 +0530

    Time: 45 ms (80.19%), Space: 19 MB (45.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,12 +3,13 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
         int low = 1;
-        long high = 1L * *max_element(piles.begin(), piles.end()) * n;
+        //as n < h, so high is only *max_element.
+        int high = *max_element(piles.begin(), piles.end());
         // cout<<high<<endl;
-        long ans = INT_MAX;
+        int ans = INT_MAX;
         
         while(low <= high){
-            long mid = low + (high - low)/2;
+            int mid = low + (high - low)/2;
             // cout<<mid<<' ';
             //check validity of mid.
             if(isEnough(piles, mid, h)){
@@ -22,13 +23,11 @@ public:
         return ans;
     }
     
-    bool isEnough(vector<int>&piles, long speed, int h){
-        int time_needed = 0;
+    bool isEnough(vector<int>&piles, int speed, int h){
+        long time_needed = 0;
         // cout<<speed<<' ';
-        for(int i=0; i<piles.size(); i++){
-            int time = (piles[i] + speed -1) / speed;
-            // cout<<time<<' ';
-            time_needed += time;
+        for(auto p: piles){
+            time_needed += (p + speed -1) / speed;
         }
         // cout<<endl;
         if(time_needed <= h) return true;
