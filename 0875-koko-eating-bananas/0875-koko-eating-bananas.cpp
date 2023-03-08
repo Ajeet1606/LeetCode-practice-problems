f8a3256130211e1959a27f2415dc419af8515c4d
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2023-03-08 16:05:42 +0530

    Time: 113 ms (5.16%), Space: 18.9 MB (45.12%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int low = 1;
+        long high = 1L * *max_element(piles.begin(), piles.end()) * n;
+        // cout<<high<<endl;
+        long ans = INT_MAX;
+        
+        while(low <= high){
+            long mid = low + (high - low)/2;
+            // cout<<mid<<' ';
+            //check validity of mid.
+            if(isEnough(piles, mid, h)){
+                ans = min(ans, mid);
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    bool isEnough(vector<int>&piles, long speed, int h){
+        int time_needed = 0;
+        // cout<<speed<<' ';
+        for(int i=0; i<piles.size(); i++){
+            int time = piles[i] % speed == 0 ? piles[i]/speed: piles[i]/speed + 1;
+            // cout<<time<<' ';
+            time_needed += time;
+        }
+        // cout<<endl;
+        if(time_needed <= h) return true;
+        return false;
+    }
+};
