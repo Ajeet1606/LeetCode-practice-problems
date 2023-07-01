2379c5f44754e41cb174fee45fdeef0221ddb55b
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2023-07-01 08:48:20 +0530

    Time: 580 ms (61.02%), Space: 171 MB (52.12%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,49 +1,52 @@
 class Solution {
-    private int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    public boolean canCross(int row, int col, int[][] cells, int day) {
-        int[][] grid = new int[row][col];
+public:
+    
+    bool canReachToEnd(vector<vector<int>>&matrix, int row, int col, int cur_row, int cur_col){
+        if(cur_row < 0 or cur_col < 0 or cur_row == row or cur_col == col) return false;
+        if(matrix[cur_row][cur_col] != 0) return false;
+        if(cur_row == row-1) return true;
         
+        //operate on this cell, move to all 4 directions.
+        matrix[cur_row][cur_col] = -1;
         
-        for (int i = 0; i < day; ++i) {
-            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
-        }
+        bool top = canReachToEnd(matrix, row, col, cur_row-1, cur_col);
+        bool left = canReachToEnd(matrix, row, col, cur_row, cur_col-1);
+        bool right = canReachToEnd(matrix, row, col, cur_row, cur_col+1);
+        bool down = canReachToEnd(matrix, row, col, cur_row+1, cur_col);
         
-        for (int i = 0; i < col; ++i) {
-            if (grid[0][i] == 0 && dfs(grid, 0, i, row, col)) {
-                return true;
-            }
-        }
-        return false;
-        
+        return top or left or right or down;
     }
-
-    private boolean dfs(int[][] grid, int r, int c, int row, int col) {
-        if (r < 0 || r >= row || c < 0 || c >= col || grid[r][c] != 0) {
-            return false;
-        }
-        if (r == row - 1) {
-            return true;
-        }
-        grid[r][c] = -1;
-        for (int[] dir : directions) {
-            int newR = r + dir[0], newC = c + dir[1];
-            if (dfs(grid, newR, newC, row, col)) {
-                return true;
+    
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        
+        int ans = 0, n = cells.size();
+        
+        int left = 0, right = n-1;
+        while(left <= right){
+            int mid = right - (right - left)/2;
+            
+            vector<vector<int>>matrix(row, vector<int>(col, 0));
+            for(int i=0; i<=mid; i++){
+                int r = cells[i][0]-1, c = cells[i][1]-1;
+                matrix[r][c] = 1;
+            }
+        
+            bool ok = false;
+            for(int j = 0; j<col; j++){
+                if(matrix[0][j] == 1) continue;
+                
+                if(canReachToEnd(matrix, row, col, 0, j)){
+                    ok = true;
+                    break;
+                }
+            }
+            if(ok){
+                left = mid+1;
+            }else{
+                right = mid-1;
             }
         }
-        return false;
-    }
-
-    public int latestDayToCross(int row, int col, int[][] cells) {
-        int left = 1, right = row * col;
-        while (left < right) {
-            int mid = right - (right - left) / 2;
-            if (canCross(row, col, cells, mid)) {
-                left = mid;
-            } else {
-                right = mid - 1;
-            }
-        }
+        
         return left;
     }
-}
+};
