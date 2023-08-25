fce647fc739edee8169133514e92956835944394
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2023-08-25 07:45:24 +0530

    Time: 3 ms (80.56%), Space: 9.1 MB (66.51%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -5,14 +5,13 @@ public:
         int n = strs.size();
         
         string ans;
-        sort(strs.begin(), strs.end());
         
         for(int i=0; i<strs[0].length(); i++){
 //             check if each string has the same char at same index.
             bool ok = true;
             
             for(int j=1; j<n; j++){
-                if(strs[j][i] != strs[0][i]){
+                if(i >= strs[j].length() or strs[j][i] != strs[0][i]){
                     ok = false;
                     break;
                 }
