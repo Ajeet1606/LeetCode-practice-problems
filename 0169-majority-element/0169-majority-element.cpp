997ef09f712fb9333a2fc6dc3c6cfebfe00ac7ad
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2023-11-09 07:47:41 +0530

    Time: 14 ms (59.50%), Space: 20 MB (53.40%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -7,15 +7,6 @@ public:
         int n = arr.size();
         sort(arr.begin(), arr.end());
         
-        int cnt = 1;
-        for(int i=1; i<n; i++){
-            if(arr[i] == arr[i-1]){
-                cnt++;
-            }else{
-                cnt=1;
-            }
-            if(cnt > n/2) return arr[i];
-        }
-        return arr[0];
+        return arr[n/2];
     }
 };
