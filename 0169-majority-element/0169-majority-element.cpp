f85f3fce4260f8fe82427cb1b594038da2b6777f
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2023-11-09 11:33:01 +0530

    Time: 16 ms (37.65%), Space: 19.8 MB (83.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,9 +4,19 @@ public:
 //         brute force: take a map, store frequency, return answer. O(N) & O(N);
         
 //         better: linear traversal with sorting. O(nlogn).
-        int n = arr.size();
-        sort(arr.begin(), arr.end());
+//         boyre moore: O(n)
+        int n = arr.size(), ans = arr[0], cnt = 1;
         
-        return arr[n/2];
+        for(int i = 1; i<n; i++){
+            if(cnt == 0){
+                ans = arr[i];
+                cnt=1;
+            }else if(arr[i] == ans){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        return ans;
     }
 };
