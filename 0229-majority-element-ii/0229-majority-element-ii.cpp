43c1afca34740f857d73e711d68fb131475caced
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2023-11-16 16:50:59 +0530

    Time: 15 ms (14.19%), Space: 16.2 MB (68.08%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& arr) {
-//         brute: use map, TC: O(n), SC: O(n)
-//         better in space: sort, O(nlogn), SC: O(1)
+//         boyre moore algorithm
+        int n = arr.size(), num1= INT_MIN, num2 = INT_MIN, cnt1 = 0, cnt2 = 0;
         
-        int n = arr.size(), cnt = 1;
-        if(n == 1) return arr;
+        for(int i=0; i<n; i++){
+            if(arr[i] == num1){
+                cnt1++;
+            }else if(arr[i] == num2){
+                cnt2++;
+            }else if(cnt1 == 0){
+                num1 = arr[i];
+                cnt1 = 1;
+            }else if(cnt2 == 0){
+                num2 = arr[i];
+                cnt2= 1;
+            }else{
+                cnt1 -=1, cnt2-=1;
+            }
+        }
+//         verify numbers;
         
-        sort(arr.begin(), arr.end());
         vector<int>ans;
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == num1) cnt1++;
+            else if(arr[i] == num2) cnt2++;
+        }
         
-        for(int i=1; i<n; i++){
-            while(i < n and arr[i-1] == arr[i]){
-                cnt++, i++;
-            }
-            if(cnt > n/3){
-                ans.push_back(arr[i-1]);
-            }
-            cnt = 1;
-        }
-        if(cnt > n/3 and ans.back() != arr[n-1]){
-            ans.push_back(arr[n-1]);
-        }
+        if(cnt1 > n/3) ans.push_back(num1);
+        if(cnt2 > n/3) ans.push_back(num2);
         
         return ans;
     }
