6d2490c0e1642a7bd733b88d5abe16520a66ea14
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2024-02-14 22:16:17 +0530

    Time: 4 ms (70.08%), Space: 12.1 MB (24.15%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>&arr1, vector<int>& arr2) {
-        int n1 = arr1.size(), n2 = arr2.size();
+    vector<int> nextGreaterElement(vector<int>& arr1, vector<int>& arr2) {
+        stack<int>s;
+        unordered_map<int, int>mp;
         vector<int>ans;
-        for(int i=0; i<n1; i++){
-            int cur = arr1[i], idx;
-            for(int j=0; j<n2; j++){
-                if(arr2[j] == arr1[i]){
-                    idx = j;
-                    break;
-                }
+        
+        for(int i=arr2.size()-1; i>=0; --i){
+            while(!s.empty() && s.top() <= arr2[i]){
+                s.pop();
             }
-            bool found = false;
-            for(int j=idx; j<n2; j++){
-                if(arr2[j] > arr1[i]){
-                    ans.push_back(arr2[j]);
-                    found = true;
-                    break;
-                }
-            }
-            if(!found) ans.push_back(-1);
+            
+            if(s.empty()) mp[arr2[i]] = -1;
+            else mp[arr2[i]] = s.top();
+            s.push(arr2[i]);
+            cout<<arr2[i];
+        }
+        
+        for(int i=0; i<arr1.size(); i++){
+            cout<<arr1[i];
+            ans.push_back(mp[arr1[i]]);
         }
         return ans;
     }
