ae111288826ec9e58fcf865b4c9681564fac1b05
Ajeet Patel <54282815+Ajeet1606@users.noreply.github.com>
2024-12-04 00:06:42 +0530

    Time: 1 ms (86.37%), Space: 14.9 MB (12.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,18 @@
 class Solution {
-public:
+public: //one paas map.
     vector<int> twoSum(vector<int>& arr, int target) {
         int len = arr.size();
         
         unordered_map<int, int>mp;
         
         for(int i=0; i<len; i++){
+            int compliment = target - arr[i];
+            if(mp.find(compliment) != mp.end()){
+                return {i, mp[compliment]};
+            }
             mp[arr[i]] = i;
         }
         
-        for(int i=0; i<len; i++){
-            int cur = arr[i];
-            int rem = target - cur;
-            if(mp.find(rem) != mp.end() && mp[rem] > i){
-                return {i, mp[rem]};
-            }
-        }
         return {};
     }
 };
